4f81e49f0483eba81052aef42c138acee0389f95
uurj <dbwjd1154@gmail.com>
2021-06-08 23:59:23 +0900

    [ Java Basic ] * 210608 * miniproject

---
+++ javaminiproject/src/com/work/util/Utility.java
@@ -0,0 +1,19 @@
+package com.work.util;
+
+public class Utility {
+
+	/**
+	 * <pre>
+	 * 현재날짜 기본형식의 문자열로 변환하는 메서드
+	 * -- 기본형식 : 년도4자리-월2자리-일2자리
+	 * </pre>
+	 * @return 현재날짜 기본형식 문자열
+	 */
+	public static String getCurrentDate() {
+		return getCurrentDate("yyyy-MM-dd");
+	}
+
+	private static String getCurrentDate(String string) {
+		return null;
+	}
+}
